30ff4a1d2d9eb5e19c3b8fa1384a2ab8a635f05e
qwernot <491298200@qq.com>
2024-02-07 12:15:20 +0800

    Site updated: 2024-02-07 12:15:17

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "city": "CN101010100",
+    "key": "d3d1058779ca4386b0b70ef32c61803d"
+  }
+}
